8e9fdb0a29a12995d044de88305b2e5839def98f
yjang43 <yjang43@wisc.edu>
2022-03-09 21:08:38 -0600

    add train code

--- dlc_train.py
+++ dlc_train.py
@@ -1,9 +1,12 @@
-from argparse import ArgumentParser
+import os
+import argparse
+import pandas as pd
 import deeplabcut
 import utils
 
+from argparse import ArgumentParser
+from deeplabcut import auxiliaryfunctions
 
-config = utils.load_config()
 
 
 
@@ -21,28 +24,52 @@ def set_args():
     parser.add_argument('--maxiters', 
                         default=100_000,
                         type=int)
+    parser.add_argument('--train',   # --no-train for False
+                        action=argparse.BooleanOptionalAction,
+                        default=True)
+    parser.add_argument('--eval',     # --no-eval for False 
+                        action=argparse.BooleanOptionalAction,
+                        default=True)
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = set_args()
+    print(args)
     
+    config = utils.load_config()
     # possibly set pose_cfg.yaml
     
+    if args.train:
     
-    deeplabcut.create_training_dataset(config=config, 
-                                       net_type=None, 
-                                       augmenter_type=None)
-    
-    deeplabcut.train_network(config,
-                             max_snapshots_to_keep=5,
-                             displayiters=args.displayiters,
-                             saveiters=args.saveiters,
-                             maxiters=args.maxiters,
-                             gputouse=args.gputouse)
-    
-    deeplabcut.evaluate_network(config,
-                            plotting=True,
-                            gputouse=args.gputouse)
+        deeplabcut.create_training_dataset(config=config, 
+                                           net_type=None, 
+                                           augmenter_type=None)
+
+        deeplabcut.train_network(config,
+                                 max_snapshots_to_keep=5,
+                                 displayiters=args.displayiters,
+                                 saveiters=args.saveiters,
+                                 maxiters=args.maxiters,
+                                 gputouse=args.gputouse)
+
+    if args.eval:
+        deeplabcut.evaluate_network(config,
+                                plotting=True,
+                                gputouse=args.gputouse)
+
+        # get the best model
+        df = pd.read_csv(os.path.join(os.path.split(config)[0], 
+                                      "evaluation-results", 
+                                      "iteration-0", 
+                                      "CombinedEvaluation-results.csv"))
+
+        maxidx = df[' Test error(px)'].idxmax()
+        print(f"Best model found with {df[' Test error(px)'][maxidx]} test error(px)")
+        auxiliaryfunctions.edit_config(config,
+                                       edits={
+                                           'snapshotindex': maxidx,
+                                       })
+
     
